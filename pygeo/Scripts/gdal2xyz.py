0b23f40203d9d4a43fe83cd5b756131157b04706
fbnpgtpp <fabien.pageot@purprojet.com>
2022-05-31 08:57:15 +0700

    k

---
+++ pygeo/Scripts/gdal2xyz.py
@@ -0,0 +1,4 @@
+#!C:/Users/pnjoya/Documents/GitHub/f_learning_geospatial_with_python/pygeo\python.exe
+# EASY-INSTALL-SCRIPT: 'GDAL==3.4.1','gdal2xyz.py'
+__requires__ = 'GDAL==3.4.1'
+__import__('pkg_resources').run_script('GDAL==3.4.1', 'gdal2xyz.py')
